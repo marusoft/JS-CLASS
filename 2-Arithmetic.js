e1ef5f7b3768e662f21db6d502b6c5eda85b65e2
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-12-10 16:29:33 +0100

    modify js class

--- 2-Arithmetic.js
+++ 2-Arithmetic.js
@@ -1,9 +1,9 @@
 console.log("Arithmetics Examples");
 
-console.log(10 + 10);
-console.log(3 - 2);
-console.log(10 * 3);
-console.log(55 / 5);
-console.log(15 % 2);
+console.log("Addition = ", 10 + 10);
+console.log("Subtraction = ", 3 - 2);
+console.log("Multiplication = ", 10 * 3);
+console.log("Divsion = ", 55 / 5);
+console.log("Modulus = ", 15 % 2);
 
 
--- 3-Inbuilt-js-method.js
+++ 3-Inbuilt-js-method.js
@@ -1,14 +1,14 @@
 console.log("JS in-built method examples");
 
 console.log("The length of my name is ", "Abdulrahmah".length);
-console.log("My name in capital letters ", "Abdulrahmah".toUpperCase());
+console.log("My name in capital letters ", "Abdulrahmah".toUpperCase())
 console.log("My name in capital letters ", "Abdulrahmah".toLowerCase());
 console.log("current time", new Date());
-console.log("Return Random Numbers btw 0 and 1", Math.random()); // return random numbers btw 0 and 1
+console.log("Return Random Numbers btw 0 and 1", Math.random()); // return random numbers btw 0 and 1 exclusive
 console.log("Return Random Numbers btw 0 and 100", Math.random() * 100);
 console.log('Return Nearest whole number', Math.floor(7.567388)); // return random numbers btw 0 and 100
 console.log('Return Nearest whole number', Math.ceil(7.167388));
 console.log('PI 22/7', Math.PI) // return random numbers btw 0 and 100
 console.log('Approximation', Math.round(60.89999))
-console.log('Raise tO power', Math.pow(2, 10))
+console.log('Two raise to power 10', Math.pow(2, 10))
 
