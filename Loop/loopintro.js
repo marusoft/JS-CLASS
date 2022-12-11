bf30652a392b0bdfb02657e2c4ce5672b711e9b1
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-12-11 12:04:23 +0100

    create loop class

--- Loop/loopintro.js
+++ Loop/loopintro.js
@@ -1,6 +1,38 @@
 // Boilerplate for "For Loop"
-for (let i = 1; i < 50; i++) {
-  const element = [i];
-  console.log(i)
-}
+// for (let i = 1; i < 50; i++) {
+//   const element = [i];
+//   console.log(i)
+// }
 
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+/**
+ * incremental loop
+ */
+// for (let i = 1; i < 11; i++) {
+//   console.log("LOOP FORWARD", i)
+// }
+/**
+ * decremental loop
+ */
+// for (let a = 10; a >= 1; a--) {
+//   console.log("LOOP BACKWARD", a)
+// }
+
+const semiFinalTeam = ["Argentina", "Croatia", "Morocco", "France"];
+const semiFinalLength = semiFinalTeam.length;
+console.log(semiFinalLength)
+
+for (let i = 0; i < semiFinalTeam.length; i++) {
+  console.log(`${semiFinalTeam[i]} is part of the team in the semi final in Qatar`);
+  // console.log(semiFinalTeam[i] + " is part of the team in the semi final in Qatar");  
+}
---
+++ Loop/nestedloop.js
@@ -0,0 +1,34 @@
+// for (let i = 0; i < array.length; i++) {
+//  for (let j = 0; j < array.length; j++) {
+//  }
+// }
+
+// 2 x 1
+// 2 x 2
+// 2 x 3
+// 2 x 4
+// 2 x 5
+// 2 x 6
+// 2 x 7
+// 2 x 8
+// 2 x 9
+// 2 x 10
+// 2 x 11
+// 2 x 12
+
+// const num1 = [3];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// for (let i = 0; i < num1.length; i++) {
+//   for (let j = 0; j < numbers.length; j++) {
+//     const answer = num1[i] * numbers[j];
+//     console.log(`${num1[i]} * ${numbers[j]} = ${answer}`);
+//   }
+// }
+const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+for (let i = 0; i < num1.length; i++) {
+  for (let j = 0; j < numbers.length; j++) {
+    const answer = num1[i] * numbers[j];
+    console.log(`${num1[i]} * ${numbers[j]} = ${answer}`);
+  }
+}
